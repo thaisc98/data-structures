5c297e2eefa9962bdbf26a9c19d1daa9dfef48d7
thaisc98 <thaisc98@hotmail.es>
2022-05-22 16:39:21 -0400

    [NEW] BFS, DFSPreOrder, DFSPostOrder and DFSInOrder

--- src/datastructures/tree/BinarySearchTree.java
+++ src/datastructures/tree/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package datastructures.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
 
     Node root; // point at the top pf the node
@@ -58,6 +62,88 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    //Breath First Search
+    public ArrayList<Integer> BFS(){
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(currentNode);
+
+        while(queue.size() > 0){
+            currentNode = queue.remove();
+            results.add(currentNode.value);
+            if(currentNode.left != null){
+                queue.add(currentNode.left);
+            }
+            if(currentNode.right != null){
+                queue.add(currentNode.right);
+            }
+        }
+        return results;
+    }
+
+    // Depth First Search - PreOrder
+    public ArrayList<Integer> DFSPreOrder(){
+        ArrayList<Integer> results = new ArrayList<>();
+        class Traverse {
+            Traverse(Node currentNode) {
+                results.add(currentNode.value);
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
+
+    // Depth First Search - PostOrder
+    public ArrayList<Integer> DFSPostOrder(){
+        ArrayList<Integer> results = new ArrayList<>();
+        class Traverse {
+            Traverse(Node currentNode) {
+
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+                // only difference of post and pre
+                results.add(currentNode.value);
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
+
+
+    public ArrayList<Integer> DFSInOrder(){
+        ArrayList<Integer> results = new ArrayList<>();
+        class Traverse {
+            Traverse(Node currentNode) {
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                // only difference of In for  pre || post
+                results.add(currentNode.value);
+
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
+
+
+
 
 }
 
--- src/datastructures/tree/Main.java
+++ src/datastructures/tree/Main.java
@@ -15,10 +15,16 @@ public class Main {
         bst.insert(52);
         bst.insert(82);
 
+
         System.out.println(bst.contains(27));
         System.out.println(bst.contains(17));
 
         //System.out.println(bst.root.left.right.value);
+
+        System.out.println(bst.BFS());
+        System.out.println(bst.DFSPreOrder());
+        System.out.println(bst.DFSPostOrder());
+        System.out.println(bst.DFSInOrder());
 
     }
 }
